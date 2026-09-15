e7c40b7afdc9e1e9c99821b04717fa857f6bce8d
yrhhsh19-art <yrhhsh19@gmail.com>
2026-09-15 10:38:49 +0300

    Change bot token in main.py
    Updated the bot token in main.py.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = '8901696840:AAF-B5e8sJ5b3XFKyHs0BZy5wvf1iT6B_fc'
+TOKEN = 8901696840:AAFFHv1mwAIbQvmdlbNOZpmx48iT5ToQGyk
 ADMIN_ID = 8287138856
 
 bot = telebot.TeleBot(TOKEN)
